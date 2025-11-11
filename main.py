825dce45264459cbf9a6e894a204a5d48f06a18f
WenC <mr.wenc2640@gmail.com>
2025-11-12 00:49:30 +0800

    [Feat]Init项目初始化

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
+
+"""
+@Project: EasyLLM
+@File   : main.py
+@Version: V1.0.0
+@Author : WenC
+@Time   : 2025-11-12 00:48:03
+@Email  : mr.wenc2640@gmail.com
+@License: (C)Copyright 2020-2030,Mr.WenC
+@Desc   :
+"""
+
+# here put the import lib
