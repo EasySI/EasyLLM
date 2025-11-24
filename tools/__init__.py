06536137e21f90b228c14a5e584cd7f960541ace
yq1320 <mr.wenc2640@gmail.com>
2025-11-24 10:05:49 +0800

    目录结构规划

---
+++ tools/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+"""
+@File   : __init__.py
+@Version: v1.0.0
+@Auther : WenC
+@Email  : mr.wenc2640@gmail.com
+@Time   : 2025-11-24 10:03:11
+@License: (C)Copyright 2025-2035, Mr.WenC
+@Desc   : 
+"""
+
+# here put the import lib
