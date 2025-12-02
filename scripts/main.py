d47d3afb5617628122c61d09c3889f48ff542ed5
WenC <mr.wenc2640@gmail.com>
2025-12-03 00:26:38 +0800

    [Feat]简单RAG示例

---
+++ scripts/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+@Project: EasyLLM
+@File   : main.py
+@Version: V1.0.0
+@Author : WenC
+@Time   : 2025-12-02 21:57:24
+@Email  : mr.wenc2640@gmail.com
+@License: (C)Copyright 2020-2030,Mr.WenC
+@Desc   :
+"""
+
+# here put the import lib
